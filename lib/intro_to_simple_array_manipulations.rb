6e0ff2b1baa2bfa03412ce093f8032c51293edf5
Kenneth Portillo <portillokenneth@gmail.com>
2019-09-02 23:03:19 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,15 @@
+def using_push(array,element)
+  array.push(element)
+end
+
+def using_unshift(array, element)
+  array.unshift(element)
+end
+
+def using_pop(array)
+  array.pop 
+end
+
+def pop_with_args(array)
+  array.pop(element, element)
+end
